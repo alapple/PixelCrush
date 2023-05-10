9ebbed05bd5b909e6b25a36e13e06569f13b2389
alapple <133106478+alapple@users.noreply.github.com>
2023-05-10 14:36:59 +0200

    remove boiler plate methods and set gamescreen

--- core/src/com/pixelcrush/game/PixelCrushCore.java
+++ core/src/com/pixelcrush/game/PixelCrushCore.java
@@ -4,28 +4,16 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.pixelcrush.game.scenes.game.GameScene;
 
 public class PixelCrushCore extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
+	public static PixelCrushCore INSTANCE;
+
+	public PixelCrushCore(){
+		INSTANCE = this;
+	}
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		setScreen(new GameScene());
 	}
 }
