b9f4d2c7ef348cf30906f01d8cd8adc36fdf0386
alapple <133106478+alapple@users.noreply.github.com>
2023-05-10 14:50:36 +0200

    added Loading Screen

--- core/src/com/pixelcrush/game/scenes/LoadingScene.java
+++ core/src/com/pixelcrush/game/scenes/LoadingScene.java
@@ -1,5 +1,25 @@
 package com.pixelcrush.game.scenes;
 
-public class LoadingScene {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.pixelcrush.game.PixelCrushCore;
+import com.pixelcrush.game.scenes.game.GameScene;
 
+public class LoadingScene extends ScreenAdapter {
+
+    public LoadingScene(){
+        PixelCrushCore.manager.load("LoadingScreen/loading-screen.png", Texture.class);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if (PixelCrushCore.manager.update()){
+            PixelCrushCore.INSTANCE.setScreen(new GameScene());
+        }
+    }
 }
