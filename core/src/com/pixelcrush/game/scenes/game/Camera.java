70a447ad70580e0a508fafa8b418b7eb8e2172ae
alapple <133106478+alapple@users.noreply.github.com>
2023-05-10 14:24:37 +0200

    created camera

--- core/src/com/pixelcrush/game/scenes/game/Camera.java
+++ core/src/com/pixelcrush/game/scenes/game/Camera.java
@@ -1,4 +1,7 @@
 package com.pixelcrush.game.scenes.game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 public class Camera {
 }
