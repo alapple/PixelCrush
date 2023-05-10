3ea7fcfbc874a873a330cbb669d2102754ebc747
alapple <133106478+alapple@users.noreply.github.com>
2023-05-10 14:38:36 +0200

    WIP cam

--- core/src/com/pixelcrush/game/scenes/LoadingScene.java
+++ core/src/com/pixelcrush/game/scenes/LoadingScene.java
@@ -1,4 +1,5 @@
 package com.pixelcrush.game.scenes;
 
 public class LoadingScene {
+
 }
--- core/src/com/pixelcrush/game/scenes/game/Camera.java
+++ core/src/com/pixelcrush/game/scenes/game/Camera.java
@@ -3,5 +3,28 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
-public class Camera {
+public class Camera{
+
+    SpriteBatch batch;
+    Player player;
+    OrthographicCamera cam;
+    public Camera(){
+        cam = new OrthographicCamera(100, 100);
+        player = new Player();
+        batch = new SpriteBatch();
+
+    }
+
+    public void camFollowPlayer(){
+        cam.position.x = player.getPositionX();
+        cam.position.y = player.getPositionY();
+        cam.update();
+    }
+
+    public void render(){
+        cam.update();
+        batch.setProjectionMatrix(cam.combined);
+    }
+
+
 }
