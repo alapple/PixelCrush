294fb8ab6750e95dd866f5c1237eb5650d9c6440
alapple <133106478+alapple@users.noreply.github.com>
2023-05-10 14:39:50 +0200

    idle fps 30, use vsync

--- desktop/src/com/pixelcrush/game/DesktopLauncher.java
+++ desktop/src/com/pixelcrush/game/DesktopLauncher.java
@@ -7,8 +7,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setIdleFPS(30);
 		config.setForegroundFPS(60);
+
 		config.setTitle("PixelCrush");
+		config.useVsync(true);
 		new Lwjgl3Application(new PixelCrushCore(), config);
 	}
 }
