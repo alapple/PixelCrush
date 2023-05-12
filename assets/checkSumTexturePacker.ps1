4992cfd3b6ab4dcdca3945f6d6d2e19e9ff577a7
justmedev <ilja.ragnar@gmail.com>
2023-05-12 09:19:58 +0200

    fix bugs in ps1 script

--- assets/checkSumTexturePacker.ps1
+++ assets/checkSumTexturePacker.ps1
@@ -8,7 +8,11 @@ foreach ($size in Get-ChildItem -Recurse ./assets/input  | Select-Object -Proper
 }
 if ($checkSum -ne $lastChecksum) {
     Write-Host "Checksums don't match... Re-compiling texture atlas"
-    java -cp runnable-texturepacker.jar com.badlogic.gdx.tools.texturepacker.TexturePacker ./assets/input ./assets/output graphics
+    Remove-Item ./assets/output
+
+    foreach ($dir in Get-ChildItem ./assets/input) {
+        java -cp ./assets/other/program-files/runnable-texturepacker.jar com.badlogic.gdx.tools.texturepacker.TexturePacker ./assets/input/$dir ./assets/output $dir
+    }
 }
 
 $checkSum | Out-File -FilePath ./assets/other/program-files/texture-packer-checksum.txt
