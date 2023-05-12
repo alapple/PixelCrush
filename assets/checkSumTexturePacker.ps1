ebc784492341e9bac6acccbe88d83781380cd919
justmedev <ilja.ragnar@gmail.com>
2023-05-12 09:35:07 +0200

    also check checksum of output dir

--- assets/checkSumTexturePacker.ps1
+++ assets/checkSumTexturePacker.ps1
@@ -1,18 +1,35 @@
 $checkSumLocation = "./assets/other/program-files/texture-packer-checksum.txt"
-$lastChecksum = Get-Content -Path $checkSumLocation
-$checkSum = 0
+$checkSumFileExists = Test-Path -Path $checkSumLocation -PathType Leaf
+if ($checkSumFileExists -eq $false)
+{
+    New-Item -Path $checkSumLocation
+}
+$lastChecksumInput, $lastChecksumOutput = Get-Content -Path $checkSumLocation
+$checkSumInput = 0
+$checkSumOutput = 0
 
 foreach ($size in Get-ChildItem -Recurse ./assets/input  | Select-Object -Property Length)
 {
-    $checkSum += [int]$size.Length
+    $checkSumInput += [int]$size.Length
 }
-if ($checkSum -ne $lastChecksum) {
-    Write-Host "Checksums don't match... Re-compiling texture atlas"
-    Remove-Item ./assets/output
 
-    foreach ($dir in Get-ChildItem ./assets/input) {
+foreach ($size in Get-ChildItem -Recurse ./assets/output  | Select-Object -Property Length)
+{
+    $checkSumOutput += [int]$size.Length
+}
+
+if ($checkSumInput -ne $lastChecksumInput -or $checkSumOutput -ne $lastChecksumOutput)
+{
+    Write-Host "Checksums don't match... Re-compiling texture atlas"
+    Remove-Item -Force -Recurse ./assets/output
+
+    foreach ($dir in Get-ChildItem ./assets/input)
+    {
         java -cp ./assets/other/program-files/runnable-texturepacker.jar com.badlogic.gdx.tools.texturepacker.TexturePacker ./assets/input/$dir ./assets/output $dir
     }
 }
 
-$checkSum | Out-File -FilePath ./assets/other/program-files/texture-packer-checksum.txt
+$checkSumInput | Out-File -FilePath ./assets/other/program-files/texture-packer-checksum.txt
+$checkSumOutput | Out-File -Append -FilePath ./assets/other/program-files/texture-packer-checksum.txt
+
+Write-Host "done!"
