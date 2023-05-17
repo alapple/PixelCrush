197b77497e005d439e0092b495c7593a5beaa1c7
justmedev <ilja.ragnar@gmail.com>
2023-05-17 19:21:30 +0200

    fix: heart typo

--- core/src/com/pixelcrush/game/scenes/game/HealthBar.java
+++ core/src/com/pixelcrush/game/scenes/game/HealthBar.java
@@ -11,23 +11,23 @@ import java.util.ArrayList;
 public class HealthBar {
     private int maxHealth = 10;
     private int health = maxHealth;
-    private Sprite emptyHearth = new Sprite();
-    private Sprite halfHearth = new Sprite();
-    private Sprite fullHearth = new Sprite();
-    private TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("output/hearth.atlas"));
+    private Sprite emptyHeart = new Sprite();
+    private Sprite halfHeart = new Sprite();
+    private Sprite fullHeart = new Sprite();
+    private TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("output/heart.atlas"));
 
 
     public HealthBar() {
-        emptyHearth = atlas.createSprite("hearth-empty");
-        halfHearth = atlas.createSprite("hearth-half");
-        fullHearth = atlas.createSprite("hearth-full");
+        emptyHeart = atlas.createSprite("heart-empty");
+        halfHeart = atlas.createSprite("heart-half");
+        fullHeart = atlas.createSprite("heart-full");
     }
 
     public ArrayList<Image> getImages() {
         ArrayList<Image> images = new ArrayList<>();
-        Sprite[] hearthsSprites = getHearthsSprites();
-        for (int i = 0; i < hearthsSprites.length; i++) {
-            Sprite sprite = hearthsSprites[i];
+        Sprite[] heartsSprites = getHeartsSprites();
+        for (int i = 0; i < heartsSprites.length; i++) {
+            Sprite sprite = heartsSprites[i];
             Image img = new Image(sprite);
             img.setScaling(Scaling.contain);
             img.scaleBy(4f);
@@ -38,15 +38,15 @@ public class HealthBar {
         return images;
     }
 
-    public Sprite[] getHearthsSprites() {
+    public Sprite[] getHeartsSprites() {
         Sprite[] hearts = new Sprite[maxHealth];
 
         for (int i = 0; i < Math.floor(health / 2f); i++) {
-            hearts[i] = fullHearth;
+            hearts[i] = fullHeart;
         }
-        if (health % 2 != 0) hearts[(int) Math.ceil(health / 2f)] = halfHearth;
+        if (health % 2 != 0) hearts[(int) Math.ceil(health / 2f)] = halfHeart;
         for (int i = 0; i < hearts.length; i++) {
-            if (hearts[i] == null) hearts[i] = fullHearth;
+            if (hearts[i] == null) hearts[i] = fullHeart;
         }
         return hearts;
     }
