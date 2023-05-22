538f1b7db7726342e55187d09c591ccddbb76414
justmedev <ilja.ragnar@gmail.com>
2023-05-22 06:27:54 +0200

    refractor enemy class

--- core/src/com/pixelcrush/game/scenes/game/Enemy.java
+++ core/src/com/pixelcrush/game/scenes/game/Enemy.java
@@ -3,33 +3,36 @@ package com.pixelcrush.game.scenes.game;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector2;
+import com.pixelcrush.game.scenes.game.enemy.SerializedEnemy;
 
 public class Enemy {
-    private final Vector2 position = new Vector2(0, 0);
-    private final Circle detectionRadius;
-    Player player;
-    private float maxVelocity;
-    private int health;
-    private int damage;
+    private Vector2 position = new Vector2();
+    private SerializedEnemy enemyData;
+    private Player player;
+    private Circle detectionCircle;
 
-    public Enemy() {
-        player = new Player();
-        detectionRadius = new Circle(position, 2);
-
+    public Enemy(Player player, SerializedEnemy enemyData) {
+        detectionCircle = new Circle(position, enemyData.followRadius);
+        this.player = player;
+        this.enemyData = enemyData;
     }
 
 
     public void followPlayer(float delta) {
-        float velocity = maxVelocity * delta;
-        if (Intersector.overlaps(detectionRadius, player.getPlayerBounds())) {
+        float velocity = enemyData.speed * delta;
+
+        if (Intersector.overlaps(detectionCircle, player.getPlayerBounds())) {
             Vector2 enemyPos = new Vector2(position);
             Vector2 playerPos = new Vector2(player.position);
             Vector2 direction = new Vector2();
+
             direction.x = (playerPos.x + 40) - (enemyPos.x + 40);
             direction.y = (playerPos.y + 40) - (enemyPos.y + 40);
             direction.nor();
+
             position.x += direction.x * velocity;
             position.y += direction.y * velocity;
+
             System.out.println(position);
         }
     }
