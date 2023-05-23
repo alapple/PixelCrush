36ce73084947dbb6fd0b4a6156d594c07fc22865
alapple <133106478+alapple@users.noreply.github.com>
2023-05-23 14:55:40 +0200

    add serialized class for all bows

---
+++ core/src/com/pixelcrush/game/scenes/game/weapons/SerializedBaseBow.java
@@ -0,0 +1,11 @@
+package com.pixelcrush.game.scenes.game.weapons;
+
+import java.util.ArrayList;
+
+public class SerializedBaseBow {
+    public String name;
+    public float shootingDistance;
+    public float damageMultiplier;
+    public String textureAtlasPath;
+    public ArrayList<String> shootingFrames;
+}
