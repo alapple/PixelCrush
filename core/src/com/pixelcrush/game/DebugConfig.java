01a2931a0ddb6608a4067db566a8560613cfa14e
justmedev <ilja.ragnar@gmail.com>
2023-05-24 08:40:33 +0200

    add button to toggle debug ui

--- core/src/com/pixelcrush/game/DebugConfig.java
+++ core/src/com/pixelcrush/game/DebugConfig.java
@@ -1,6 +1,6 @@
 package com.pixelcrush.game;
 
 public class DebugConfig {
-    public static final boolean DEBUG_RENDER = true;
+    public static boolean DEBUG_RENDER = true;
     public static final boolean SKIP_LOADING = true;
 }
--- core/src/com/pixelcrush/game/scenes/game/DebugUI.java
+++ core/src/com/pixelcrush/game/scenes/game/DebugUI.java
@@ -1,18 +1,35 @@
 package com.pixelcrush.game.scenes.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.pixelcrush.game.DebugConfig;
 import com.pixelcrush.game.PixelCrushCore;
 
 public class DebugUI {
     private final Skin uiSkin = PixelCrushCore.manager.get("data/uiskin.json");
     private final Label fpsLabel = new Label("Collecting...", uiSkin);
+    private final TextButton showDebugButton = new TextButton("Show Debug UI", uiSkin, "toggle");
 
     public DebugUI(Stage stage) {
+        showDebugButton.setChecked(DebugConfig.DEBUG_RENDER);
+        showDebugButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                DebugConfig.DEBUG_RENDER = showDebugButton.isChecked();
+            }
+        });
+        stage.addActor(showDebugButton);
         stage.addActor(fpsLabel);
         handleResize(0, Gdx.graphics.getHeight());
+    }
+
+    public void update() {
+
     }
 
     public void updateFPSText() {
@@ -20,6 +37,7 @@ public class DebugUI {
     }
 
     public void handleResize(int width, int height) {
-        fpsLabel.setPosition(0, height - 25);
+        fpsLabel.setPosition(0, height - fpsLabel.getHeight() - 5);
+        showDebugButton.setPosition(width - showDebugButton.getWidth() - 5, height - showDebugButton.getHeight() - 5);
     }
 }
